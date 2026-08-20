3969b3f30e0d39c667e835213e84a8ff82348d53
Rostislav Svoboda <rosta.svo@gmail.com>
2026-08-20 09:41:34 +0200

    Drop Qute module as String Templates has been removed in JDK 23

--- jdk21/qute/src/main/java/io/quarkus/ts/jdk21/preview/records/RecordPreviewShowcase.java
+++
@@ -1,4 +0,0 @@
-package io.quarkus.ts.jdk21.preview.records;
-
-public record RecordPreviewShowcase(Object value1, Object value2, Object value3, Object value4) {
-}
--- jdk21/qute/src/main/java/io/quarkus/ts/jdk21/preview/records/WebTemplatePreview.java
+++
@@ -1,6 +0,0 @@
-package io.quarkus.ts.jdk21.preview.records;
-
-import io.quarkus.qute.TemplateInstance;
-
-public record WebTemplatePreview(String headerOne, String customText) implements TemplateInstance {
-}
